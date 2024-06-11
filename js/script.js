14dd65362f97d84a647a3c30b77b155062a3c32b
Shadow <mdurihin@gmail.com>
2024-06-11 12:07:03 +0300

    debug

--- js/script.js
+++ js/script.js
@@ -1,14 +1,11 @@
 function copyText() {
-    // Находим элемент по ID
+
     var textToCopy = document.getElementById("textToCopy");
     
-    // Выделяем текст
     textToCopy.select();
-    textToCopy.setSelectionRange(0, 99999); // Для мобильных устройств
+    textToCopy.setSelectionRange(0, 99999); 
     
-    // Копируем текст
     document.execCommand("copy");
     
-    // Сообщаем пользователю об успешном копировании
     alert("Текст скопирован: " + textToCopy.value);
 }
