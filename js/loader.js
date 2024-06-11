76d94f4ad82da3821b5eaa62ba934b839b25d26a
Shadow <mdurihin@gmail.com>
2024-06-11 12:02:35 +0300

    Add loader and fix some bugs

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
-// window.addEventListener("load", function() {
-//     setTimeout(function() {
-//         document.querySelector(".loader").style.display = "none";
-//         document.getElementById("content").style.display = "block";
-//         document.body.style.overflow = "auto";
-//     }, 4100); 
-// });
+window.addEventListener("load", function() {
+    setTimeout(function() {
+        document.querySelector(".loader").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        document.body.style.overflow = "auto";
+    }, 2100); 
+});
