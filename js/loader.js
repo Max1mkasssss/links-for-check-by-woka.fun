5004298c8575d13319c7af78cea904f7b5d83c48
Shadow <mdurihin@gmail.com>
2024-06-11 09:39:02 +0300

    debug

--- js/loader.js
+++ js/loader.js
@@ -1,7 +1,7 @@
-window.addEventListener("load", function() {
-    setTimeout(function() {
-        document.querySelector(".loader").style.display = "none";
-        document.getElementById("content").style.display = "block";
-        document.body.style.overflow = "auto"; // Возвращаем скролл после загрузки
-    }, 4100); // 2 секунды
-});
+// window.addEventListener("load", function() {
+//     setTimeout(function() {
+//         document.querySelector(".loader").style.display = "none";
+//         document.getElementById("content").style.display = "block";
+//         document.body.style.overflow = "auto";
+//     }, 4100); 
+// });
