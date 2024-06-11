86277473ae24ad4cdc839ef2c5b5f112860205d9
Shadow <mdurihin@gmail.com>
2024-06-11 19:19:21 +0300

    loader

--- js/loader.js
+++ js/loader.js
@@ -1,8 +1,8 @@
-// window.addEventListener("load", function() {
+window.addEventListener("load", function() {
     
-//     setTimeout(function() {
-//         document.querySelector(".loader").style.display = "none";
-//         document.getElementById("content").style.display = "block";
-//         document.body.style.overflow = "auto";
-//     }, 2100); 
-// });
+    setTimeout(function() {
+        document.querySelector(".loader").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        document.body.style.overflow = "auto";
+    }, 2100); 
+});
